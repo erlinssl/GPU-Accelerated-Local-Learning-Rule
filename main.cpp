fec0b8325c923e0e31b05553003ac58fee397e35
erlinssl <erlinssl@stud.ntnu.no>
2022-01-28 13:44:43 +0100

    Checkpoint. Working on matplotlib functionality

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@
 #include "Model.h"
 
 #include "dependencies/matplotlibcpp.h"
+namespace plt = matplotlibcpp;
 
 double figsize_scale = 0.2;
+// TODO Figure out how to set rcParams in matplotlib-cpp
 
-// TODO matplotlib.cpp  AND  import MNIST dataset
+
+// TODO Import MNIST dataset
 
 std::vector<std::vector<std::vector<double>>> get_batch(size_t batch_size){
     // TODO Functionality dependent on MNIST implementation
@@ -30,16 +33,26 @@ void experiment(const char* subfigure, double sigma, double lambda_, size_t batc
     // np.save(f"figure2{subfigure}.npy", model.W) // python version
 }
 
-int figure(const CubeArray images){  // TODO change return type -> return (fig, axes)
+plt::Plot figure(const CubeArray& images){  // TODO Change return type -> return (fig, axes)
+    const int nrows = (int) std::sqrt(images.w.size()), ncols = (int) std::sqrt(images.w.size());
+    for(size_t i = 0; i < images.w.size(); i++){
+        plt::imshow(images[i], 5, 5);
+    }
+    return plt::Plot();
 }
 
 void save_all(){
+    std::vector<char> subfigures;
+    subfigures = {'a', 'b', 'c', 'd'};
+    for (char fig : subfigures){
+        CubeArray testArray(true, 16, 5, 5);
+        figure(testArray);
+    }
 }
 
 
-
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const double learning_rate = .1;
+    save_all();
     return 0;
 }
